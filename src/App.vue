22e30cef4bab8b411a5fc20ec5bbf4b2d51a6507
NaeemRadan <naeem.radan@gmail.com>
2025-08-19 20:35:51 +0300

    feat: Создана структура страниц и настроен роутер

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/incomes">Доходы</router-link> |
+    <router-link to="/orders">Заказы</router-link> |
+    <router-link to="/sales">Продажи</router-link> |
+    <router-link to="/stocks">Склады</router-link>
   </nav>
   <router-view/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
+/* يمكننا إضافة بعض التنسيق البسيط هنا */
 nav {
   padding: 30px;
+  text-align: center;
 }
 
 nav a {
   font-weight: bold;
   color: #2c3e50;
+  margin: 0 10px;
 }
 
 nav a.router-link-exact-active {
-  color: #42b983;
+  color: #42b983; /* اللون الأخضر الخاص بـ Vue */
 }
 </style>
---
+++ src/views/StocksView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Страница "Склады" (Stocks)</h1>
+</template>
